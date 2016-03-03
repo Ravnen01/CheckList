a83ff57ecb349290367a140b758b38d9be57a800
Corentin LECOMTE <lecomte.corentin@gmail.com>
2016-03-03 12:01:31 +0100

    création interface addItem

---
+++ CheckList/CheckList/AddItemViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddItemViewController.swift
+//  CheckList
+//
+//  Created by Corentin LECOMTE on 03/03/2016.
+//  Copyright © 2016 LECOR. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+   
+    @IBAction func done(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: {
+            
+        })
+    }
+
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: {
+            
+        })
+    }
+}
